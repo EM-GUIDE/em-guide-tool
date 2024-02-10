efe184971f966601c56aca4f6725c13d1a29ec87
revnandi <revnandi@gmail.com>
2024-02-10 09:36:12 +0100

    ignore dist and add url to server config

--- dist/src/emails/base.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EmailBaseTemplate = void 0;
-const jsx_runtime_1 = require("react/jsx-runtime");
-const mjml_react_1 = require("@faire/mjml-react");
-const EmailBaseTemplate = ({ header, body, footer, }) => {
-    return ((0, jsx_runtime_1.jsx)(mjml_react_1.Mjml, { children: (0, jsx_runtime_1.jsxs)(mjml_react_1.MjmlBody, { width: 500, children: [header, (0, jsx_runtime_1.jsx)(mjml_react_1.MjmlSection, { children: body }), (0, jsx_runtime_1.jsx)(mjml_react_1.MjmlSection, { children: footer })] }) }));
-};
-exports.EmailBaseTemplate = EmailBaseTemplate;
--- dist/src/emails/header.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EmailHeaderTemplate = void 0;
-const jsx_runtime_1 = require("react/jsx-runtime");
-const mjml_react_1 = require("@faire/mjml-react");
-const EmailHeaderTemplate = ({ title, preview, }) => {
-    return ((0, jsx_runtime_1.jsxs)(mjml_react_1.MjmlHead, { children: [(0, jsx_runtime_1.jsx)(mjml_react_1.MjmlTitle, { children: title }), (0, jsx_runtime_1.jsx)(mjml_react_1.MjmlPreview, { children: preview })] }));
-};
-exports.EmailHeaderTemplate = EmailHeaderTemplate;
