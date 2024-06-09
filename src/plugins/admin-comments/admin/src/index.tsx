6b73bbd721adbca8d71ae97454441825e7e63d32
revnandi <revnandi@gmail.com>
2024-06-09 22:17:54 +0200

    remove comments menu link

--- src/plugins/admin-comments/admin/src/index.tsx
+++ src/plugins/admin-comments/admin/src/index.tsx
@@ -11,26 +11,6 @@ const name = pluginPkg.strapi.name;
 
 export default {
   register(app: any) {
-    app.addMenuLink({
-      to: `/plugins/${pluginId}`,
-      icon: PluginIcon,
-      intlLabel: {
-        id: `${pluginId}.plugin.name`,
-        defaultMessage: name,
-      },
-      Component: async () => {
-        const component = await import("./pages/App");
-
-        return component;
-      },
-      permissions: [
-        // Uncomment to set the permissions of the plugin here
-        // {
-        //   action: '', // the action name should be plugin::plugin-name.actionType
-        //   subject: null,
-        // },
-      ],
-    });
     const plugin = {
       id: pluginId,
       initializer: Initializer,
