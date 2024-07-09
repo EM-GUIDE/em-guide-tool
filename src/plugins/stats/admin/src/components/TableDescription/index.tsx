118d0b8f09ba686d0ca2a22fa2ae6e7afca1f448
revnandi <revnandi@gmail.com>
2024-07-09 13:31:31 +0200

    change stat label size

--- src/plugins/stats/admin/src/components/TableDescription/index.tsx
+++ src/plugins/stats/admin/src/components/TableDescription/index.tsx
@@ -3,7 +3,7 @@ import { Flex, Typography } from "@strapi/design-system";
 export const TableDescription = ({ text }: { text: string }) => {
   return (
     <Flex justifyContent="center" marginBottom={2}>
-      <Typography fontSize={1} variant="beta" fontweight="bold" as="h4">
+      <Typography fontSize={2} variant="beta" fontweight="bold" as="h3">
         {text}
       </Typography>
     </Flex>
